3d35fb44d9c5a729a023caed1c8cee9eeb74d9cc
MxDx <maxime.dxl2002@gmail.com>
2023-12-23 13:32:41 +0100

    Blob done

--- project/src/back-end/products/src/utils/crud/crud-products.js
+++ project/src/back-end/products/src/utils/crud/crud-products.js
@@ -1,33 +1,89 @@
 const products = require('nano')(process.env.DB_URL);
 
-const createProduct = (product) => {
-  return new Promise((resolve, reject) => {
-    products.insert(
-      product,
-      (error, success) => {
-        if (success) {
-          const productWIthId = { ...product, _id: success.id, _rev: success.rev };
-          resolve({product : productWIthId});
-        } else {
-          reject(new Error(`Error creating an product. Reason: ${error.reason}.`));
-        }
-      }
-    );
-  });
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+const blobService = BlobServiceClient.fromConnectionString(process.env.AZURE_CONNECTION_STRING);
+
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const createProduct = (product, req) => {
+  const imageUrl = product.image;
+  const imagePath = path.resolve(__dirname, 'images', `${product._id}.png`);
+  return downloadImage(imageUrl, imagePath)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        products.insert(
+          product,
+          (error, success) => {
+            if (success) {
+              const productWithId = { ...product, _id: success.id, _rev: success.rev };
+              uploadImageToBlobStorage(imagePath, productWithId._id, req)
+                .then(() => resolve({product : productWithId}))
+                .catch(reject);
+            } else {
+              reject(new Error(`Error creating a product. Reason: ${error.reason}.`));
+            }
+          }
+        );
+      });
+    });
 }
 
-const updateProduct = (product) => {
+const updateProduct = (product, req) => {
+  const imageUrl = product.image;
+  const imagePath = path.resolve(__dirname, 'images', `${product._id}.png`);
+  return downloadImage(imageUrl, imagePath)
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        products.insert(
+          product,
+          (error, success) => {
+            if (success) {
+              uploadImageToBlobStorage(imagePath, product._id, req)
+                .then(() => resolve({product : product}))
+                .catch(reject);
+            } else {
+              reject(new Error(`Error updating a product. Reason: ${error.reason}.`));
+            }
+          }
+        );
+      });
+    });
+}
+
+const downloadImage = (url, path) => {
+  return axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+      }),
+  );
+}
+
+const uploadImageToBlobStorage = (imagePath, productId, req) => {
   return new Promise((resolve, reject) => {
-    products.insert(
-      product,
-      (error, success) => {
-        if (success) {
-          resolve({product : product});
-        } else {
-          reject(new Error(`Error updating an product. Reason: ${error.reason}.`));
-        }
-      }
-    );
+    const blobName = `${productId}.png`;
+    const containerClient = blobService.getContainerClient('scapp-product');
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    blockBlobClient.uploadFile(imagePath)
+      .then(() => {
+        fs.unlink(imagePath, (err) => {
+          if (err) {
+            console.error(`Error deleting file ${imagePath}.`, err);
+          }
+        });
+        resolve();
+      })
+      .catch(reject);
   });
 }
 
